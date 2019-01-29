87b3383aca1ef897eb3bc483ef6af45299c4133d
Elias Bachaalany <lallousz-x86@yahoo.com>
2019-01-28 17:14:59 -0800

    added 'Wizmo' project

---
+++ Wizmo/StdAfx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__F4F494B0_AC5C_478C_A723_73FB5BE76182__INCLUDED_)
+#define AFX_STDAFX_H__F4F494B0_AC5C_478C_A723_73FB5BE76182__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#define _WIN32_WINNT 0x0500
+
+#include <stdio.h>
+#include <windows.h>
+#include <mmsystem.h>
+#include <tchar.h>
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__F4F494B0_AC5C_478C_A723_73FB5BE76182__INCLUDED_)
