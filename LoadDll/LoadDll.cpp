7307d016f8d7b68c160f49b1d0935505fddd3fef
Elias Bachaalany <elias.bachaalany@gmail.com>
2021-04-17 18:05:19 -0700

    Fixed bug in loading multiple DLLs

--- LoadDll/LoadDll.cpp
+++ LoadDll/LoadDll.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <stdio.h>
 
+//-------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -10,42 +11,45 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    size_t count = argc - 1;
-    HMODULE *hMods = new HMODULE[count];
-    char **dll_names = new char *[count];
+    size_t nMaxDlls  = size_t(argc) - 1;
+    size_t nLoadDlls = 0;
+    HMODULE *hMods   = new HMODULE[nMaxDlls];
+    char **dll_names = new char *[nMaxDlls];
 
-    for (size_t i=0;i<count;++i)
+    // Loading stage
+    for (size_t iArg=1, iMod=0; iArg < argc; ++iArg)
     {
-        const char *dll_name = argv[i + 1];
+        const char *dll_name = argv[iArg];
 
         __try
         {
             HMODULE hMod = LoadLibraryA(dll_name);
             if (hMod == nullptr)
             {
-                printf("#%zu failed to load '%s'", i, dll_name);
-                return -2;
+                printf("#%zu failed to load '%s'...skipped", iArg, dll_name);
+                continue;
             }
 
-            printf("#%zu %p: '%s' loaded...\n", i, hMod, dll_name);
+            printf("#%zu %p: '%s' loaded...\n", iArg, hMod, dll_name);
 
-            hMods[i] = hMod;
-            dll_names[i] = _strdup(dll_name);
+            hMods[iMod] = hMod;
+            dll_names[iMod] = _strdup(dll_name);
+            ++iMod;
         }
         __except (EXCEPTION_EXECUTE_HANDLER)
         {
-            printf("#%zu exception occurred!", i);
-            _getch();
-            break;
+            printf("#%zu exception occurred while loading '%s'...skipped", iArg, dll_name);
+            (void)_getch();
+            continue;
         }
     }
 
     // Pause and unload
-    printf("\npress any key to terminate and unload...");
-    _getch();
+    printf("\nPress any key to terminate and unload...");
+    (void)_getch();
     printf("\n");
 
-    for (size_t i=0;i<count;++i)
+    for (size_t i=0; i < nLoadDlls; ++i)
     {
         HMODULE hMod = hMods[i];
         FreeLibrary(hMod);
