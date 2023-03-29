7174f711ed399835b111a8ebae4011872e49bb53
Elias Bachaalany <elias.bachaalany@gmail.com>
2023-03-28 19:06:48 -0700

    Create prep-cmake.bat

---
+++ Wizmo/prep-cmake.bat
@@ -0,0 +1,31 @@
+@echo off
+
+:: check the Batchography book
+
+setlocal
+
+if not exist build (
+    mkdir build
+    pushd build
+    cmake .. -A Win32
+    popd
+)
+
+if not exist build64 (
+    mkdir build64
+    pushd build64
+    cmake -A x64 ..
+    popd
+)
+
+if "%1"=="build" (
+    pushd build
+    cmake --build . --config Release
+    pushd ..\build64
+    cmake --build . --config Release
+    popd
+)
+
+echo.
+echo All done!
+echo.
