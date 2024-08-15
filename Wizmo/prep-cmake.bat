bb70ab8df2a17c1789e88bca256c33f455977caa
Elias Bachaalany <elias.bachaalany@gmail.com>
2024-08-14 18:56:28 -0700

    Update prep-cmake.bat

--- Wizmo/prep-cmake.bat
+++ Wizmo/prep-cmake.bat
@@ -4,26 +4,13 @@
 
 setlocal
 
-if not exist build (
-    mkdir build
-    pushd build
-    cmake .. -A Win32
-    popd
-)
+if not exist build cmake .. -A Win32 -B build
 
-if not exist build64 (
-    mkdir build64
-    pushd build64
-    cmake -A x64 ..
-    popd
-)
+if not exist build64 cmake -A x64 .. -B build64
 
 if "%1"=="build" (
-    pushd build
-    cmake --build . --config Release
-    pushd ..\build64
-    cmake --build . --config Release
-    popd
+    cmake --build build --config Release
+    cmake --build build64 --config Release
 )
 
 echo.
